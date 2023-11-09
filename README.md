4877dc4aedf5827ed64d53b43245a6e86e6eebc2
Gernot Pansy <gernot.pansy@ut11.net>
2023-11-09 11:11:05 +0100

    Initial version

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
-# update-deployment-status-by-unreleased-commits
+# Github action to update deployment status by unreleased commits
+
+This action retrieves the new commits since a specific tag, parses the commits for a specific format and updates the deployment status for all matching commits. This action is designed to use with [[fluxcd]] and github commit status notification provider.
+
+## Inputs
+
+### `token`
+
+**Required** The github token to be used to make the deployment status update api requests. Default `"${{ github.token }}"`.
+
+### `repo-regex`
+
+**Required** The regex to match the repo for the deployment status update. Default `"deploy (\w+)"`.
+
+## `repo-regex`
+
+**Required** The regex to match the repo for the deployment status update. Default `"deploy (\w+)"`.
+
+## `tag`
+
+**Required** The tag to use for getting the unreleased commits. Default `"deployed"`.
+
+
+## Outputs
+
+### 
+
+## Example usage
+
+```yaml
+name: Updates deployment statuses on github in other repositories
+on:
+  - status
+
+jobs:
+  check:
+    runs-on: ubuntu-latest
+    if: github.event.state == 'success'
+    steps:
+      - uses: actions/checkout@v4
+        with:
+            fetch-depth: 0
+      - uses: notz/update-deplyoment-status-by-unreleased-commits
+        with:
+            repo-regex: 'Deploy (\w+)'
+            deployment-id-regex: '\[([0-9]+)\]'
+            tag: 'deployed'
+      - name: Tag Repo
+        uses: richardsimko/update-tag@v1
+        with:
+          tag_name: deployed
+        env:
+          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+```
+
+
+## Build action
+
+```
+./node_modules/.bin/ncc build index.js --license licenses.txt
+```
---
+++ index.js
@@ -0,0 +1,113 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+import { execFile } from 'child_process';
+import { Readable } from 'stream';
+import through from 'through2';
+
+try {
+    await main();
+} catch (error) {
+    core.setFailed(error.message);
+}
+
+const main = async () => {
+    const inputs = parseInputs();
+  
+    const commits = await getCommits(inputs.tag);
+    core.debug(commits);
+
+    const repoRegex = new RegExp(inputs.repoRegex);
+    const deploymentIdRegex = new RegExp(inputs.deploymentIdRegex);
+
+    const octokit = github.getOctokit(inputs.token);
+
+    let count = 0;
+    commits.forEach((commit) => {
+        let repoMatches = repoRegex.exec(commit);
+        let deploymentIdMatches = deploymentIdRegex.exec(commit);
+        if (repoMatches && deploymentIdMatches) {
+            updateDeploymentStatus(octokit, repoMatches[1], deploymentIdMatches[1], 'success');
+            count++;
+        }
+    });
+  
+    core.setOutput('count', count);
+};
+
+const parseInputs = () => {
+    return {
+        token: getInput('token') || process.env.GITHUB_TOKEN,
+        repoRegex: getInput('repo-regex'),
+        deploymentIdRegex: getInput('deployment-id-regex'),
+        tag: getInput('tag') || "deployed",
+    };
+};
+
+const run = async (cmd, args) => {
+    const readable = new Readable();
+    readable._read = () => { };
+  
+    let isError = false;
+  
+    const child = execFile(cmd, args, {
+      maxBuffer: Infinity
+    });
+  
+    child.stdout
+      .pipe(through((chunk, enc, cb) => {
+        readable.push(chunk);
+        isError = false;
+        cb();
+      }, (cb) => {
+        setImmediate(() => {
+          if (!isError) {
+            readable.push(null);
+            readable.emit('close');
+          }
+          cb();
+        })
+      }));
+  
+    child.stderr
+      .pipe(through.obj((chunk) => {
+        isError = true;
+        readable.emit('error', new Error(chunk));
+        readable.emit('close');
+      }));
+  
+    let data = '';
+    for await (const chunk of readable) {
+      data += chunk;
+    }
+    return data;
+}
+
+const getCommits = async (tag) => {
+    const cmd = `git`;
+    const range = `${tag}..HEAD`;
+    const args = ['log', range, `--format={{filter:%B}}online//`].filter((s) => {
+      return s !== '';
+    });
+    const result = await run(cmd, args);
+  
+    return result.split('//\n').map((s) => {
+      return s.replace(/{{filter:.+?}}/gs, '');
+    });
+};
+
+const updateDeploymentStatus = async (octokit, repo, deployment_id, state, auto_inactive = true) => {
+    const owner = github.context.repo.owner;
+
+    const req = {
+        owner,
+        repo,
+        deployment_id,
+        state,
+        auto_inactive,
+    };
+
+    core.debug(JSON.stringify(req));
+    const resp = await octokit.request('POST /repos/{owner}/{repo}/deployments/{deployment_id}/statuses', req);
+    core.debug(JSON.stringify(resp));
+};
